606df8bc7c9264f371a1bba433ac71154f15d953
Hugo Junior <hjunioreng@gmail.com>
2023-01-18 21:20:56 -0300

    gerando senha criptografada e token

--- src/app.js
+++ src/app.js
@@ -1,55 +1,95 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import { MongoClient } from "mongodb";
-import dotenv from "dotenv"
-import joi from 'joi'
-
+import dotenv from "dotenv";
+import joi from 'joi';
+import bcrypt from "bcrypt"
+import {v4 as uuid} from "uuid";
 
 dotenv.config();
 
-const app =express();
 
+const app = express()
+;
 app.use(cors());
 app.use(express.json());
 
- const mongoClient = new MongoClient(process.env.REACT_APP_API_URL)
- let db
+const mongoClient = new MongoClient(process.env.REACT_APP_API_URL);
+let db;
 
- mongoClient.connect()
- .then(() =>{
-     db = mongoClient.db()
- })
+mongoClient.connect()
+    .then(() => {
+        db = mongoClient.db()
+    });
 
- app.post("/", async (req,res)=>{
-const {email, password} =req.headers
+app.post("/", async (req, res) => {
+    const { email, password } = req.headers;
 
-console.log(email,"email")
-console.log(password,"password")
+    const userSchema = joi.object({
+        email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'br'] } }).required(),
+        password: joi.string().min(6).max(18).required()
+    });
 
-const userSchema =joi.object({
-    email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'br'] } }).required(),
-    password: joi.string().min(6).max(18).required()
-})
+    const validation = userSchema.validate({ email, password }, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    };
 
-try{
+    try {
 
-const validation = userSchema.validate({email, password}, {abortEarly: false})
-if(validation.error){
-    const errors = validation.error.details.map((detail)=> detail.message);
-    return res.status(422).send(errors)
-}
-    
-    await db.collection("users").insertOne({
-        email:email,
-        password:password
-    })
+        const user = await db.collection("users").findOne({ email });
 
-    return res.sendStatus(201)
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = uuid();
+            
+            await db.collection("sessions"),insertOne({
+                userId: user._id,
+                token
+            })
+            res.send(token)
+            
+        } else {
+            return res.status(422).send("e-mail ou senha incorretos")
+        }
 
-} catch(err){return res.status(500).send(err.massage)}
+        return res.sendStatus(201);
 
- } )
+    } catch (err) { return res.status(500).send(err.message) }
+});
 
-const PORT = process.env.PORT_SERVER
+app.post("/cadastro", async (req, res) => {
+    const { username, email, password, repeatPassword } = req.body;
 
-app.listen(PORT, ()=>{ console.log(`Servidor rodando na porta ${PORT}`)})
+    const userSchema = joi.object({
+
+        username: joi.string().required(),
+        email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'br'] } }).required(),
+        password: joi.string().min(6).max(18).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+        repeatPassword: joi.ref('password')
+    });
+
+    const validation = userSchema.validate({ username, email, password, repeatPassword }, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    };
+
+    try {
+
+        const passwordHash = bcrypt.hashSync(password, 10);
+
+        await db.collection("users").insertOne({
+            username: username,
+            email: email,
+            password: passwordHash,
+        });
+        return res.sendStatus(201);
+
+    } catch (err) { return res.status(500).send(err.message) };
+
+});
+
+const PORT = process.env.PORT_SERVER;
+
+app.listen(PORT, () => { console.log(`Servidor rodando na porta ${PORT}`) });
